858784c0a3b8d6d52a05e01de3fa39f78e28d4d0
roxyta <roxys13@gmail.com>
2014-10-11 20:28:00 -0500

    commit fizzBuzz

---
+++ fizzBuzz/Numeros.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzBuzz
+{
+  public  class Numeros
+    {
+      public string tipo(int num)
+      {
+          var res = "";
+
+          for (var i = 0; i < num; i++)
+          {
+              if (num % 5 == 0 & num % 3 == 0)
+              {
+                  res = "FizzBuzz";
+              }
+              else if (num % 3 == 0)
+              {
+                  res = "Fizz";
+              }
+              else if (num % 5 == 0)
+              {
+                  res = "Buzz";
+              }
+              else
+              {
+                  res = num.ToString();
+              }
+          }
+
+
+          return res;
+      }
+
+    }
+}
---
+++ fizzBuzzTest/FizzBuzzTestt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using fizzBuzz;
+
+
+namespace fizzBuzzTest
+{
+  public  class FizzBuzzTestt
+    {
+      //evalua cualquier numero
+
+      [Test]
+      public void NumerosaEvaluar()
+      {
+          var numero = new Numeros();
+          var tipo = numero.tipo(99);
+          Assert.AreEqual("Fizz", tipo);
+      }
+
+
+    
+      /*Evaluacion de numero por cada tipo
+
+      [Test]
+      public void TestMultiploDeTres()
+      {
+          var numero = new Numeros();
+          var tipo = numero.tipo(99);
+          Assert.AreEqual("Fizz", tipo);
+
+      }
+
+      [Test]
+      public void TestMultiploDeCinco()
+      {
+          var numero = new Numeros();
+          var tipo = numero.tipo(100);
+          Assert.AreEqual("Buzz", tipo);
+
+      }
+
+
+      [Test]
+      public void TestGuarismos()
+      {
+          var numero = new Numeros();
+          var tipo = numero.tipo(15);
+          Assert.AreEqual("FizzBuzz", tipo);
+
+      }
+      [Test]
+      public void TestOtroNumero()
+      {
+          var numero = new Numeros();
+          var tipo = numero.tipo(4);
+          Assert.AreEqual("4", tipo);
+
+      }
+
+      */
+
+    }
+}
